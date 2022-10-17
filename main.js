a383b06e85af7a47396d85f0cfaf9a71be63ea23
Islomjon-2013 <hulkarsiddiqova747@gmail.com>
2022-10-17 15:53:47 -0400

    added js codes

---
+++ main.js
@@ -0,0 +1,94 @@
+"user strict";
+
+//variables
+//let number = 10;
+//const name = "hulkar";
+//console.log(number);
+//number = 20;
+//console.log(number);
+//const color = ["red", "green", "apple"];
+//console.log(color[1]);
+//const home = {
+//window: "black",
+// door: "white",
+//};
+
+//const place = confirm("are you here?");
+//console.log(place);
+//const currentPlace = prompt("where are you?", "");
+//console.log(currentPlace);
+//const age = +prompt("how old are you?"); //stringni numberga aylantirish uchun + ni qo'shib qo'yamiz
+//console.log(age);
+//const favorityColor = [];
+//favorityColor[0] = prompt("what is your favority color #1", "");
+//favorityColor[1] = prompt("what is your favority color #2", "");
+//favorityColor[2] = prompt("what is your favority color #3", "");
+//console.log(favorityColor);
+//console.log(typeof favorityColor);
+//interpolatsiya;
+//const channel = prompt("whats your favoruty youtuber");
+
+//console.log(`https://youtube.com/${channel}`);
+//const user = "hulkar";
+//const job = "web developer";
+//console.log(`user name is ${user},she is ${job}.`);
+//opertorlar
+//console.log(6 + +"9");
+//console.log(6 + "9");
+//inc decr
+//let incr = 10;
+//let decr = 10;
+
+//console.log(incr++);
+//console.log(decr--);
+//console.log(10 % 3); //qoldig'ini ko'rsatadi 10 ni 3 ga bo'lganda 1ta qoldig' qoladi
+//console.log(5 * 5 == "25");
+//console.log(5 * 5 === "25");
+//const isAge = false;
+//const isClose = false;
+//console.log(isAge && isClose); va
+//console.log(isAge || isClose);
+//console.log(!isAge);
+//console.log(2 * 3 + 6 !== 10);
+//let numberOfDB = +prompt("how many game did you see?", "5");
+//et seriesDB = {
+// count: numberOfDB,
+//series: {},
+//actors: {},
+//genres: [],
+//privat: false,
+////};
+//const a = prompt("ohirgi korgan serilingiz?"),
+// b = prompt("nechchi haho berasiz"),
+//c = prompt("ohirgi korgan serilingiz?"),
+// d = prompt("nechchi haho berasiz");
+//seriesDB.series[a] = b;
+//seriesDB.series[c] = d;
+//console.log(seriesDB);
+
+//if else conditions
+//const age = +prompt("how old are you?", "");
+//if (age > 25) {
+//console.log("horor film");
+//} else if (age > 18 && age < 25) {
+// console.log("bayevik film");
+//} else {
+//console.log("cartoons");
+//}
+//ternary operator
+//age > 25 ? console.log("komedy") : console.log("cartoon");
+//switch case
+//const color = "green";
+//switch (color) {
+//case "red":
+// console.log("stop");
+//break;
+//case "green":
+// console.log("go");
+//break;
+// case "yellow":
+// console.log("go slowly");
+// break;
+//default:
+//console.log("trafik");
+//}
